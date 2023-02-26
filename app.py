d79d2f9ee192edeeeec1c9fe8e4dbe9085299020
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2023-02-27 01:16:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import re
 #     cv2.waitKey(0)
 #     cv2.destroyAllWindows()
 
-st.markdown("<h1 style='text-align: center; '>Ansar Chan (ﾉ◕ヮ◕) 解</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; '>Ansar Chan (◕ヮ◕) 解</h1>", unsafe_allow_html=True)
 a=st.text_input("Enter array of questions: ")
 
 
