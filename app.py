a2a78a3ed61847155c8fca2eb32ef4d8a730fe61
Eeman1113 <eeman.majumder@gmail.com>
2023-03-05 02:07:53 +0530

    anal

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if cus:
     if cus_fon is None:
         st.sidebar.write("Waiting for da font......")
         st.sidebar.write("Take your time upload...here is sample output while I wait") 
-        st.sidebar.markdown("![Alt Text](Image/hitopotato-yawn.gif)")
+        st.sidebar.markdown("![Alt Text](https://tenor.com/en-GB/view/hitopotato-yawn-sleepy-tired-wanna-sleep-gif-15343889)")
         cus_fon="fonts/Mynerve-Regular.ttf"
         cus = False
         agree = True
